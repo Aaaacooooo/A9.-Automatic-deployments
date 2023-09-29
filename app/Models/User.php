88113507c09f8a3366b603fbd6c57a9000b4e900
Aaaacooooo <acoray06@gmail.com>
2023-09-29 22:21:20 +0100

    name included

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array<int, string>
      */
     protected $fillable = [
-        'user_id', 'channel_id', 'title', 'link', 'approved'
+        'user_id', 'channel_id', 'title', 'link', 'approved','name'
     ];
 
     /**
